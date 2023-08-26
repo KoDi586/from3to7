fcd878b6838f4688785408631e3c0af1b2941649
1Malevolence1 <danil36rusdanil@ mail.ru>
2023-08-26 23:09:27 +0300

    end

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class Main {
+
+    public static int[] givefrom3to7(int[] ints) {
+        int[] finishints = new int[5];
+        for (int i = 2; i <= 6; i++) {
+            if (ints[i] % 2 == 0) {
+                finishints[i - 2] = ints[i] + 1;
+            } else {
+                finishints[i - 2] = ints[i];
+            }
+        }
+        return finishints;
+    }
+
+    public static void main(String[] args) {
+        int[] begin = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] finish = givefrom3to7(begin);
+        System.out.println(Arrays.toString(finish));
     }
 }
